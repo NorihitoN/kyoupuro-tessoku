b1025cd11ad59304e3a28ad4aea6ec105dbe8aa9
norihito <norihito0626@gmail.com>
2023-02-07 23:18:43 +0900

    added a13

---
+++ scala/a13.scala
@@ -0,0 +1,12 @@
+// import scala.io.StdIn._
+import java.util.Scanner
+
+object Main extends App {
+  val sc = new Scanner(System.in)
+  val n = sc.nextInt()
+  val k = sc.nextInt()
+  val a = Vector.fill(n){sc.nextInt()}
+
+  
+
+}
