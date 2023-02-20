0ad517bba69be7fed2b85f94c755d70d26f3f49b
norihito <norihito0626@gmail.com>
2023-02-20 21:45:21 +0900

    a26.cpp

--- cpp/a26.cpp
+++ cpp/a26.cpp
@@ -21,38 +21,13 @@ int main()
     
     for (int i = 2; i*i <= n; i++) {
         if (deleted[i] == true) continue;
-        for (int j = i * 2; i * j <= n; j++) deleted[i*i] = true;
-    }
-    
-    cin >> h >> w;
-
-    for (int i = 1; i <= h; i++) {
-        for (int j = 1; j <= w; j++) {
-            cin >> c[i][j];
-        }
+        for (int j = i * 2; j <= n; j += i) deleted[j] = true;
     }
 
-    dp[1][1] = 1;
-    for (int j = 2; j <= w; j++) {
-        if (c[1][j] == '#') dp[1][j] = 0;
-        else dp[1][j] = dp[1][j-1];
+    for (int i = 1; i <= q; i++)
+    {
+        if (deleted[x[i]] == false) cout << "Yes" << endl;
+        else cout << "No" << endl;
     }
-
-    
-    for (int i = 2; i <= h; i++) {
-        for (int j = 1; j <= w; j++) {
-            if (c[i][j] == '#') dp[i][j] = 0;
-            else {
-                if (i == 1) {
-                    dp[i][j] = dp[i][j-1];
-                } else if (j == 1) {
-                    dp[i][j] = dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
-        }
-    }
-    cout << dp[h][w];
     return 0;
 }
